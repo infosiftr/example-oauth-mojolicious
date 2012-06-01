7f9a21cbc0e5a3eba4b8031e71f05571639d8afa
Andrew Page <andrew@infosiftr.com>
2012-06-01 17:27:26 -0600

    updated a bunch of the service provider code - request_token should be mostly working, authorize might be mostly working as well (we're saving tokens and nonces to MongoDB correctly now)

--- service-provider.pl
+++ service-provider.pl
@@ -7,6 +7,7 @@ use sort 'stable';
 use Mojolicious::Lite;
 use Net::OAuth;
 use MongoDB; # for token and nonce storage
+use Tie::IxHash;
 
 $Net::OAuth::PROTOCOL_VERSION = Net::OAuth::PROTOCOL_VERSION_1_0A;
 
@@ -25,10 +26,17 @@ if (@ARGV > 1) {
 	);
 }
 
+my $database = shift || 'example-oauth-mojolicious';
+
 helper db => sub {
 	my $self = shift;
 	
-	return MongoDB->new;
+	state $db = MongoDB::Connection->new(
+		host => 'localhost',
+		port => 27017,
+	)->get_database($database);
+	
+	return $db;
 };
 
 helper handleRequest => sub {
@@ -58,17 +66,33 @@ helper handleRequest => sub {
 	
 	$request->consumer_secret($consumers{$request->consumer_key});
 	
-	# TODO check database to ensure nonce/timestamp combination is unique
-	
 	# if a token is provided and a token_secret is required, look up the secret from the database
 	if ($request->{token} && grep { $_ eq 'token_secret' } @{ $request->required_api_params }, @{ $request->required_message_params }) {
-		my $token = $request->{token};
-		
-		# TODO look up token_secret from the database
+		$request->token_secret(
+			$self->db->oauthTokens->find_one({
+					_id => $request->{token},
+				}, { secret => 1 })->{secret}
+		);
 	}
 	
 	unless ($request->verify) {
 		die 'invalid signature';
+	}
+	
+	tie my %nonceId, 'Tie::IxHash'; # we have to use an IxHash here to ensure we have consistent ordering of fields for MongoDB, since {nonce: '...', timestamp: '...'} is a different key from {timestamp: '...', nonce: '...'}
+	
+	# I considered just making it a string and concatenating the two for simplicity, but that would've changed the semantics, since them anything that *concatentates* to the same nonce-timestamp value would be invalid, which isn't technically true
+	
+	$nonceId{nonce} = $request->nonce;
+	$nonceId{timestamp} = $request->timestamp;
+	
+	unless ($self->db->oauthNonces->insert({
+				_id => \%nonceId,
+				providerTimestamp => time,
+				token => $request->{token} || undef,
+				consumerKey => $request->consumer_key,
+			}, { safe => 1 })) {
+		die 'invalid nonce/timestamp'; # already been used
 	}
 	
 	return $request;
@@ -105,10 +129,22 @@ get '/oauth/request_token' => sub {
 	
 	my $request = $self->handleRequest('Request Token');
 	
-	my $token = $self->generateRandomString;
-	my $secret = $self->generateRandomString;
+	my ($token, $secret);
 	
-	# TODO save token into database
+	do {
+		$token = $self->generateRandomString;
+		$secret = $self->generateRandomString;
+	} until (
+		$self->db->oauthTokens->insert({
+				_id => $token,
+				secret => $secret,
+				type => 'request',
+				consumerKey => $request->consumer_key,
+				createTimestamp => time,
+				lastUsedTimestamp => 0,
+				callback => $request->callback,
+			}, { safe => 1 })
+	);
 	
 	$self->handleResponse(
 		'Request Token',
@@ -121,7 +157,49 @@ get '/oauth/request_token' => sub {
 get '/oauth/authorize' => sub {
 	my $self = shift;
 	
-	# TODO
+	my $requestToken = $self->db->oauthTokens->find_one({
+			_id => $self->param('oauth_token'),
+			type => 'request',
+		}, {
+			_id => 1,
+			consumerKey => 1,
+			callback => 1,
+		});
+	
+	unless ($requestToken) {
+		die 'invalid token';
+	}
+	
+	# I only check this because I'm overly pedantic
+	unless ($consumers{$requestToken->{consumerKey}}) {
+		die 'invalid/revoked consumer';
+	}
+	
+	# this is where a "login"/"authorize this application to view your data" page would be presented to the user - we'll just pretend that that happened and move on with the interesting stuff
+	
+	my $userId = 123; # yay, we're logged in ;)
+	
+	my $verifier = $self->generateRandomString;
+	
+	$self->db->oauthTokens->update({
+			_id => $requestToken->{_id},
+			type => $requestToken->{type},
+		}, {
+			'$set' => {
+				lastUsedTimestamp => time,
+				verifier => $verifier,
+				userId => $userId,
+			},
+		}, {
+			safe => 1,
+		});
+	
+	my $callback = Mojo::URL->new($requestToken->{callback});
+	$callback->query->append(
+		oauth_token => $requestToken->{_id},
+		oauth_verifier => $verifier,
+	);
+	$self->redirect_to($callback);
 };
 
 get '/oauth/access_token' => sub {
@@ -129,7 +207,16 @@ get '/oauth/access_token' => sub {
 	
 	my $request = $self->handleRequest('Access Token');
 	
-	# TODO
+	my $requestToken = $self->db->find_one({
+			_id => $request->{token},
+			type => 'request',
+		}, {
+			_id => 1,
+			consumerKey => 1,
+			callback => 1,
+		});
+	
+	...;
 };
 
 get '/api/ping' => sub {
@@ -137,7 +224,7 @@ get '/api/ping' => sub {
 	
 	my $request = $self->handleRequest('Protected Resource');
 	
-	# TODO
+	...;
 	
 	$self->render(json => { pong => time });
 };
