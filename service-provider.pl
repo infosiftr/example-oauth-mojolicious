fb32c5e742d34ae39154c03006d19ba4ead5d94d
Andrew Page <andrew@infosiftr.com>
2012-06-01 10:22:08 -0600

    basic skeleton of a service provider (obviously not yet fleshed out)

---
+++ service-provider.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+
+use Mojolicious::Lite;
+use Net::OAuth;
+use MongoDB; # for token and nonce storage
+
+$Net::OAuth::PROTOCOL_VERSION = Net::OAuth::PROTOCOL_VERSION_1_0A;
+
+my $listen = shift || 'http://*:3000';
+
+my %consumers = (
+	# key => secret
+	asdf => 'jkl;',
+);
+
+if (@ARGV > 1) {
+	my $consumerKey = shift;
+	my $consumerSecret = shift;
+	%consumers = (
+		$consumerKey => $consumerSecret,
+	);
+}
+
+helper db => sub {
+	my $self = shift;
+	
+	return MongoDB->new;
+};
+
+get '/oauth/request_token' => sub {
+	my $self = shift;
+	
+	# TODO
+};
+
+get '/oauth/authorize' => sub {
+	my $self = shift;
+	
+	# TODO
+};
+
+get '/oauth/access_token' => sub {
+	my $self = shift;
+	
+	# TODO
+};
+
+get '/api/ping' => sub {
+	my $self = shift;
+	
+	# TODO
+	
+	$self->render(json => { pong => time });
+};
+
+app->start(qw(daemon -l), $listen);
